e1200d4f61378282dc9ac96af9fd435bdcc294c9
aryanpradhan1 <109923956+aryanpradhan1@users.noreply.github.com>
2023-10-09 14:12:57 -0500

    thing

--- Chapter 15 Activities/FloodFill/src/Grid.java
+++ Chapter 15 Activities/FloodFill/src/Grid.java
@@ -1,15 +1,30 @@
+import java.util.Stack;
+
 public class Grid
 {
     private static final int SIZE = 10;
     int[][] pixels = new int[SIZE][SIZE];
-    . . .
+    Stack<Pair> pairs = new Stack<>();
 
     /**
      * Flood fill, starting with the given row and column.
     */
     public void floodfill(int row, int column)
     {
-        . . .
+        int count = 1;
+        Pair first = new Pair(row, column);
+        pairs.push(first);
+
+        while(pairs.size() > 0) {
+            Pair p = pairs.pop();
+            if (pixels[p.getRow()][p.getColumn()] == 0){
+                pixels[p.getRow()][p.getColumn()] = count;
+                count++;
+            }
+        }
+
+        if(p.getRow() + 1 && pixels)
+
     }
 
     @Override
